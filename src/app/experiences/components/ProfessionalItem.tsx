38dfd24b8830e1e4c530ebf59e77d68dfdb523df
ChiElephant <anthony.merino@me.com>
2023-04-19 03:34:35 -0600

    fixed professional item href link

--- src/app/experiences/components/ProfessionalItem.tsx
+++ src/app/experiences/components/ProfessionalItem.tsx
@@ -18,7 +18,7 @@ export default async function ProfessionalItem({
 
 	return (
 		<li className='group'>
-			<Link href='link'>
+			<Link href={link}>
 				<div className='flex w-full gap-4 rounded-md p-2 duration-200 group-hover:scale-105 group-hover:shadow-md'>
 					<div className='relative mt-1 flex h-12 w-12 flex-none items-center justify-center overflow-clip rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0'>
 						<Image
