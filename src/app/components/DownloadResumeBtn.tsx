688729e7eedb1d898544ad675b656e259856482b
ChiElephant <anthony.merino@me.com>
2023-05-05 02:07:11 -0600

    add new resume file

--- src/app/components/DownloadResumeBtn.tsx
+++ src/app/components/DownloadResumeBtn.tsx
@@ -5,7 +5,7 @@ export default function DownloadResumeBtn() {
 	return (
 		<div className='my-10 flex justify-center'>
 			<Link
-				href='/Anthony_Merino_resume.pdf'
+				href='/Anthony_Merino_Resume.pdf'
 				locale={false}
 				target='_blank'
 				rel='noopener noreferrer'
